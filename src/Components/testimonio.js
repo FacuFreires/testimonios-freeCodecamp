7489a5570cbc03158a9047af006954d8cffdc906
Facundo Freires <facufreires1@gmail.com>
2022-05-16 18:18:46 -0300

    Commit Clon testimonios freeCodeCamp

--- src/Components/testimonio.js
+++ src/Components/testimonio.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import '../stylesheets/testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
    return (
       <div className='contenedor-testimonio'>
          <img 
          className='imagen-testimonio' 
-         src={require('../img/testimonio-Emma.png')}
+         src={require(`../img/testimonio-${props.imagen}.png`)}
          alt='imagen de Emma' />
+         <div className='contenedor-texto-testimonio'>
+         <p className='nombre-testimonio'>
+         <strong>{props.nombre}</strong> en {props.pais}
+         </p>
+
+         <p className='cargo-testimonio'>
+            {props.cargo} en <strong>{props.empresa}</strong>
+         </p>
+
+         <p className='texto-testimonio'>{props.testimonio}</p>
+         </div>
       </div>
    );
 }
+
+export default Testimonio;
